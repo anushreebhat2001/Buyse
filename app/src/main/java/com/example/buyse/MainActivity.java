46deb35cdc9a216a5b38e7f25360fae16c51f0df
Kaustubh BM <kaustubhbm3574@gmail.com>
2022-04-08 23:17:33 +0530

    Login Functionality
    Still need to add database stuff.

--- app/src/main/java/com/example/buyse/MainActivity.java
+++ app/src/main/java/com/example/buyse/MainActivity.java
@@ -6,13 +6,38 @@ import android.widget.TextView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+   Button b1;
+   EditText ed1,ed2;
 
-        textView = findViewById(R.id.Signup);
-    }
 
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+
+      ed1 = (EditText)findViewById(R.id.editTextTextEmailAddress);
+      ed2 = (EditText)findViewById(R.id.editTextTextPassword);
+
+      b1 = (Button)findViewById(R.id.button2);
+       
+      b1.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            if(ed1.getText().toString().equals("admin") &&
+               ed2.getText().toString().equals("thisisarandompassword")) {
+                  Toast.makeText(getApplicationContext(),
+                     "Login Successful",Toast.LENGTH_SHORT).show();
+//                 Changes to the Homepage after successful login
+//                 Need to create a BUYSE HOME class
+               Intent intent = new Intent(MainActivity.this, BUYSE_Home.class);
+               startActivity(intent);
+               }
+             else{
+                  Toast.makeText(getApplicationContext(), "Wrong 
+                     Credentials",Toast.LENGTH_SHORT).show();
+               }
+         }
+      });
+
+   }
 }

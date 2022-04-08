2a8654536e18c1808891f0c3fb1e2e9023f5d9a4
Kaustubh BM <kaustubhbm3574@gmail.com>
2022-04-08 23:27:17 +0530

    Updating Signup.java

--- app/src/main/java/com/example/buyse/SignUp.java
+++ app/src/main/java/com/example/buyse/SignUp.java
@@ -1,4 +1,35 @@
 package com.example.buyse;
 
 public class SignUp {
+Button b1;
+   EditText ed1,ed2;
+
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+
+      ed1 = (EditText)findViewById(R.id.editTextTextEmailAddress);
+      ed2 = (EditText)findViewById(R.id.editTextTextPassword);
+
+      b1 = (Button)findViewById(R.id.button2);
+       
+      b1.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            if(ed1.getText().toString()!="" &&
+               ed2.getText().toString()!="") {
+                  Toast.makeText(getApplicationContext(),
+                     "Registration Successful",Toast.LENGTH_SHORT).show();
+//               Need to send the registered data into a db
+              
+//               This changes back to login page
+               Intent intent = new Intent(SignUp.this, MainActivity.class);
+               startActivity(intent);
+            }
+         }
+      });
+
+   }
 }

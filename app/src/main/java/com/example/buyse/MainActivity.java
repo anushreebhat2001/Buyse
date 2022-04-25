5f8af5a08f9a4825d4f4e9059ccbd34df329ccfb
Kaustubh BM <kaustubhbm3574@gmail.com>
2022-04-25 20:02:22 +0530

    Fixed gradle stuff it was quite fucky before Added functionality for Register and Login The switching between MainActivity to Signup Activity does not work for some reason. The storing of email,pass and login signup works

--- app/src/main/java/com/example/buyse/MainActivity.java
+++ app/src/main/java/com/example/buyse/MainActivity.java
@@ -1,43 +1,112 @@
 package com.example.buyse;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.*;
 import android.os.Bundle;
+import android.content.*;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
    Button b1;
    EditText ed1,ed2;
-
+   private FirebaseAuth mAuth;
+   TextView signup_redirect;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
-
+      mAuth = FirebaseAuth.getInstance();
       ed1 = (EditText)findViewById(R.id.editTextTextEmailAddress);
       ed2 = (EditText)findViewById(R.id.editTextTextPassword);
-
+      signup_redirect = (TextView)findViewById(R.id.SignupRedirect);
       b1 = (Button)findViewById(R.id.button2);
-       
-      b1.setOnClickListener(new View.OnClickListener() {
+
+//      Switching between intents does not work but login and signup functionality works
+
+      signup_redirect.setOnClickListener(new View.OnClickListener() {
          @Override
-         public void onClick(View v) {
-            if(ed1.getText().toString().equals("admin") &&
-               ed2.getText().toString().equals("thisisarandompassword")) {
-                  Toast.makeText(getApplicationContext(),
-                     "Login Successful",Toast.LENGTH_SHORT).show();
-//                 Changes to the Homepage after successful login
-//                 Need to create a BUYSE HOME class
-               Intent intent = new Intent(MainActivity.this, BUYSE_Home.class);
-               startActivity(intent);
-               }
-             else{
-                  Toast.makeText(getApplicationContext(), "Wrong 
-                     Credentials",Toast.LENGTH_SHORT).show();
-               }
+         public void onClick(View view) {
+            Intent intent2 = new Intent (MainActivity.this, SignUp.class);
+            startActivity(intent2);
          }
       });
 
+      b1.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            loginUserAccount();
+         }
+      });
+   }
+
+
+   private void loginUserAccount()
+   {
+
+
+      // Take the value of two edit texts in Strings
+      String email, password;
+      email =ed1.getText().toString();
+      password = ed2.getText().toString();
+
+      // validations for input email and password
+      if (TextUtils.isEmpty(email)) {
+         Toast.makeText(getApplicationContext(),
+                 "Please enter email!!",
+                 Toast.LENGTH_LONG)
+                 .show();
+         return;
+      }
+
+      if (TextUtils.isEmpty(password)) {
+         Toast.makeText(getApplicationContext(),
+                 "Please enter password!!",
+                 Toast.LENGTH_LONG)
+                 .show();
+         return;
+      }
+
+      mAuth.signInWithEmailAndPassword(email, password)
+              .addOnCompleteListener(
+                      new OnCompleteListener<AuthResult>() {
+                         @Override
+                         public void onComplete(
+                                 @NonNull Task<AuthResult> task)
+                         {
+                            if (task.isSuccessful()) {
+                               Toast.makeText(getApplicationContext(),
+                                       "Login successful!!",
+                                       Toast.LENGTH_LONG)
+                                       .show();
+
+
+                               // if sign-in is successful
+                               // intent to home activity
+                               Intent intent
+                                       = new Intent(MainActivity.this,
+                                       BuyseHome.class);
+                               startActivity(intent);
+                            }
+
+                            else {
+
+                               // sign-in failed
+                               Toast.makeText(getApplicationContext(),
+                                       "Login failed!!",
+                                       Toast.LENGTH_LONG)
+                                       .show();
+                            }
+                         }
+                      });
    }
 }
--- app/src/main/java/com/example/buyse/SignUp.java
+++ app/src/main/java/com/example/buyse/SignUp.java
@@ -1,35 +1,113 @@
 package com.example.buyse;
 
-public class SignUp {
-Button b1;
-   EditText ed1,ed2;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.*;
+import android.os.Bundle;
+import android.content.*;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class SignUp extends AppCompatActivity {
+   Button b1;
+   EditText ed1,ed2;
+   FirebaseAuth mAuth;
+   TextView LoginRedirect;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_main);
-
+      setContentView(R.layout.signup);
+      mAuth = FirebaseAuth.getInstance();
       ed1 = (EditText)findViewById(R.id.editTextTextEmailAddress);
       ed2 = (EditText)findViewById(R.id.editTextTextPassword);
-
+      LoginRedirect = (TextView)findViewById(R.id.LoginRedirect);
       b1 = (Button)findViewById(R.id.button2);
-       
+
+      LoginRedirect.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+            Intent intent = new Intent(SignUp.this, MainActivity.class);
+            startActivity(intent);
+         }
+      });
+
       b1.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
-            if(ed1.getText().toString()!="" &&
-               ed2.getText().toString()!="") {
-                  Toast.makeText(getApplicationContext(),
-                     "Registration Successful",Toast.LENGTH_SHORT).show();
-//               Need to send the registered data into a db
-              
-//               This changes back to login page
-               Intent intent = new Intent(SignUp.this, MainActivity.class);
-               startActivity(intent);
-            }
+            registerNewUser();
          }
       });
 
    }
+   private void registerNewUser()
+   {
+
+      // show the visibility of progress bar to show loading
+
+      // Take the value of two edit texts in Strings
+      String email, password;
+      email = ed1.getText().toString();
+      password = ed2.getText().toString();
+
+      // Validations for input email and password
+      if (TextUtils.isEmpty(email)) {
+         Toast.makeText(getApplicationContext(),
+                 "Please enter email!!",
+                 Toast.LENGTH_LONG)
+                 .show();
+         return;
+      }
+      if (TextUtils.isEmpty(password)) {
+         Toast.makeText(getApplicationContext(),
+                 "Please enter password!!",
+                 Toast.LENGTH_LONG)
+                 .show();
+         return;
+      }
+
+      // create new user or register new user
+      mAuth
+              .createUserWithEmailAndPassword(email, password)
+              .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                 @Override
+                 public void onComplete(@NonNull Task<AuthResult> task)
+                 {
+                    if (task.isSuccessful()) {
+                       Toast.makeText(getApplicationContext(),
+                               "Registration successful!",
+                               Toast.LENGTH_LONG)
+                               .show();
+
+
+                       // if the user created intent to login activity
+                       Intent intent
+                               = new Intent(SignUp.this,
+                               MainActivity.class);
+                       startActivity(intent);
+                    }
+                    else {
+
+                       // Registration failed
+                       Toast.makeText(
+                               getApplicationContext(),
+                               "Registration failed!!"
+                                       + " Please try again later",
+                               Toast.LENGTH_LONG)
+                               .show();
+
+                    }
+                 }
+              });
+   }
+
 }
+
+
+
